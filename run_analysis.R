bc57be0a8449f2d8246b0b3d0bcc153a16952417
Rakshithshetty96 <rakshithshetty96@gmail.com>
2017-10-23 11:54:33 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,52 @@
+## Get the data and store it in a data folder
+if(!file.exists("./data"))
+{       
+        dir.create("./data")
+}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")# method=curl is not used here for windows
+
+##unzip the zip file into a directory
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+##Check the list of files 
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+files
+
+##Read the files into local variables
+
+##Read the Activity files
+dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+##Read the Subject files
+
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+##Read Fearures files
+
+dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+
+## Combine the datasets of Train and Test datasets
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+
+##Check the names of dataset
+names(dataSubject)
+
+##Set the names to the variables
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+names(dataFeatures)<- dataFeaturesNames$V2
+
+
+##Merge the datasets
+dataTemp<-cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataTemp)
+
+
+
 
